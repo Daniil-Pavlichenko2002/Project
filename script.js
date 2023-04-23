2ca63e6d31c081a06437f9c8e5f7c43818e6ad95
Daniil-Pavlichenko2002 <pavlichenko.02@internet.ru>
2023-04-23 16:47:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector('.header__burger').addEventListener('click', function() {
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.header__shadow').classList.toggle('open');
+    document.querySelector('.header__meny').classList.toggle('open');
+    document.querySelector('body').classList.toggle('noscroll');
+    document.querySelector('.footer').classList.toggle('footer__fixed');
+    document.querySelector('.services__button').classList.toggle('button__fixed');
+    })
+document.querySelector('.header__shadow').addEventListener('click', function() {
+    document.querySelector('.header__shadow').classList.remove('open');
+    document.querySelector('.header__meny').classList.toggle('open');
+    document.querySelector('.header__burger').classList.remove('active');
+    document.querySelector('body').classList.remove('noscroll');
+    document.querySelector('.footer').classList.remove('footer__fixed');
+    document.querySelector('.services__button').classList.remove('button__fixed');
+})
+
